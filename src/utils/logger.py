0316e522d94e1d66adecf9ae9f83023219c52e1b
Joxos <johndoe@example.com>
2024-05-12 13:17:23 +0800

    update

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,10 +1,6 @@
-"""
-Logging utils and DI for game events.
-"""
 from loguru import logger
 import sys
 from utils.config import LOG_LEVEL
-
 
 logger.remove()
 logger.add(sys.stderr, level=LOG_LEVEL, colorize=True)
